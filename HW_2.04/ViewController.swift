64d46c1035dceb0c787ed61636b07964f84bc2a6
biibolzulpukarov <biybol07@gmail.com>
2023-05-27 10:26:49 +0200

    Initial Commit

---
+++ HW_2.04/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_2.04
+//
+//  Created by Бийбол Зулпукаров on 27/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
